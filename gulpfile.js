63fbefeb3c50e912080a58cef90872124c57d5cb
Mathieu <math.gamache@gmail.com>
2016-10-14 16:59:01 -0400

    Add todo where we should modify thing to support sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,50 +3,46 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
+// Get new module : you can search at http://gulpjs.com/plugins/ (don't forget it should be saved in package.json)
 
-/* Defines globs to target specic files type */
+// Todo: need to add new path to target .scss files
 var paths = {
   scripts: ['src/js/**/*.js', '!src/external/**/*.js'],
   images: 'src/img/**/*'
 };
- 
-/* Register some tasks to expose to the cli */
+
+// Todo: Need to run the sass task when building
 gulp.task('build', gulp.series(
   clean,
   gulp.parallel(scripts, images)
 ));
+
 gulp.task(clean);
 gulp.task(watch);
- 
-// The default task (called when you run `gulp` from cli) 
 gulp.task('default', gulp.series('build', watch));
  
  
-/* Define our tasks using plain functions */
- 
-// Will delete the directory name build
 function clean() {
-  // Return a promise: (gulp required that the function return a promise, a stream or alternatively take a call back and call it)
   return del(['build']);
 }
  
-// Take all images find in paths.images, minify them and save each of them in "build/img" (initial one doesn't change)
 function images() {
   return gulp.src(paths.images)
-    // imagemin is the module required at the beginning of the files, some module can take option
     .pipe(imagemin({optimizationLevel: 5}))
     .pipe(gulp.dest('build/img'));
 }
  
-// Minify all JavaScript (except vendor scripts) and save the concatanate result in "build/js"
 function scripts() {
   return gulp.src(paths.scripts)
     .pipe(uglify())
     .pipe(concat('all.min.js'))
     .pipe(gulp.dest('build/js'));
 }
+
+// Todo: add a function to compile sass into css
+
  
-// Rerun the task when a file changes 
+// Todo: Watch for modification in scss files
 function watch() {
   gulp.watch(paths.scripts, scripts);
   gulp.watch(paths.images, images);
